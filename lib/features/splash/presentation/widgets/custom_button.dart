4b5db0d0ed3de18095b651d3f4e7575c486911b7
doaa gamal <djmal6644@gmail.com>
2024-10-12 13:30:26 +0300

    last update

---
+++ lib/features/splash/presentation/widgets/custom_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final Color backgroundColor;
+  final Color textColor;
+  final double fontSize;
+  final double horizontalPadding;
+  final double verticalPadding;
+
+  const CustomButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+    this.backgroundColor = Colors.blue,
+    this.textColor = Colors.white,
+    this.fontSize = 18.0,
+    this.horizontalPadding = 100.0,
+    this.verticalPadding = 15.0,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor, // Button color
+        padding: EdgeInsets.symmetric(
+          horizontal: horizontalPadding,
+          vertical: verticalPadding,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      onPressed: onPressed,
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: fontSize,
+          color: textColor, // Text color
+        ),
+      ),
+    );
+  }
+}
