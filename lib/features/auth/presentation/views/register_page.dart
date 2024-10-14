e66382a4f8ed3bc9c4ed9f0bab1b8713c93f7345
doaa gamal <djmal6644@gmail.com>
2024-10-14 10:00:37 +0300

    update login and register

--- lib/features/auth/presentation/views/register_page.dart
+++ lib/features/auth/presentation/views/register_page.dart
@@ -8,6 +8,7 @@ import 'package:task_project/features/auth/presentation/model_view/users_model.d
 import '../../../../core/functions/show_snack_bar.dart';
 import '../../../../core/widgets/custom_button.dart';
 import '../../../home/presentation/views/home_page.dart';
+
 class RegisterPage extends StatefulWidget {
   @override
   State<RegisterPage> createState() => _RegisterPageState();
@@ -17,22 +18,24 @@ class _RegisterPageState extends State<RegisterPage> {
   final _formKey = GlobalKey<FormState>();
 
   final TextEditingController _nameController = TextEditingController();
-
   final TextEditingController _emailController = TextEditingController();
-
   final TextEditingController _passwordController = TextEditingController();
-
   final TextEditingController _rePasswordController = TextEditingController();
-@override
-void dispose() {
-    // TODO: implement dispose
-  _nameController.dispose();
-  _emailController.dispose();
-  _passwordController.dispose();
-  _rePasswordController.dispose();
+
+  bool _isPasswordVisible = false;
+  bool _isRePasswordVisible = false;
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _rePasswordController.dispose();
     super.dispose();
   }
-  bool isLoading = false ;
+
+  bool isLoading = false;
+
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<AuthCubit, AuthStates>(
@@ -43,134 +46,131 @@ void dispose() {
           isLoading = false;
           Navigator.pushReplacement(
             context,
-            MaterialPageRoute(builder: (context) =>const HomePage()),
+            MaterialPageRoute(builder: (context) => const HomePage()),
           );
         } else if (state is RegisterFailure) {
           isLoading = false;
           showSnackBar(context, state.errorMessages);
         }
       },
-
-    builder : (context, state)=> ModalProgressHUD(
-
-      inAsyncCall: isLoading,
-      child: Scaffold(
-      appBar: AppBar(
-      title:const  Text(
-        'Register',
-        style: TextStyle(
-          fontSize: 24,
-          fontFamily: 'Pacifico',
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
+      builder: (context, state) => ModalProgressHUD(
+        inAsyncCall: isLoading,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'Register',
+              style: TextStyle(
+                fontSize: 24,
+                fontFamily: 'Pacifico',
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            backgroundColor: Colors.blue[900],
+            centerTitle: true,
+          ),
+          body: SingleChildScrollView(
+            child: Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Colors.blue[900]!, Colors.blue[100]!],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const SizedBox(height: 50),
+                          const Text(
+                            'Create an Account',
+                            style: TextStyle(
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          const SizedBox(height: 30),
+                          _buildTextField('Name', false, _nameController, (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Name is required';
+                            }
+                            if (!RegExp(r'^[a-zA-Z]+$').hasMatch(value)) {
+                              return 'Name must contain only letters';
+                            }
+                            return null;
+                          }),
+                          const SizedBox(height: 20),
+                          _buildTextField('Email', false, _emailController, (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Email is required';
+                            }
+                            if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                              return 'Enter a valid email';
+                            }
+                            return null;
+                          }),
+                          const SizedBox(height: 20),
+                          _buildPasswordField('Password', _passwordController, (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Password is required';
+                            }
+                            if (value.length < 8) {
+                              return 'Password must be at least 8 characters long';
+                            }
+                            return null;
+                          }),
+                          const SizedBox(height: 20),
+                          _buildPasswordField('Re-Password', _rePasswordController, (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Re-Password is required';
+                            }
+                            if (value != _passwordController.text) {
+                              return 'Passwords do not match';
+                            }
+                            if (value.length < 8) {
+                              return 'Re-Password must be at least 8 characters long';
+                            }
+                            return null;
+                          }),
+                          const SizedBox(height: 40),
+                          CustomButton(
+                            text: 'Register',
+                            backgroundColor: Colors.grey,
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                final userdata = UserModel(
+                                  _nameController.text.trim(),
+                                  _emailController.text.trim(),
+                                  _passwordController.text.trim(),
+                                );
+                                BlocProvider.of<AuthCubit>(context).registerUser(userdata: userdata);
+                              }
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
-      backgroundColor: Colors.blue[900],
-      centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-      child: Stack(
-      children: [
-      Container(
-      decoration: BoxDecoration(
-      gradient: LinearGradient(
-      colors: [Colors.blue[900]!, Colors.blue[100]!],
-      begin: Alignment.topCenter,
-      end: Alignment.bottomCenter,
-      ),
-      ),
-      ),
-      Center(
-      child: Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Form(
-      key: _formKey,
-      child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-      const   SizedBox(height: 50),
-      const  Text(
-      'Create an Account',
-      style: TextStyle(
-      fontSize: 28,
-      fontWeight: FontWeight.bold,
-      color: Colors.white,
-      ),
-      ),
-      const  SizedBox(height: 30),
-      _buildTextField('Name', false, _nameController, (value) {
-      if (value == null || value.isEmpty) {
-      return 'Name is required';
-      }
-      if (!RegExp(r'^[a-zA-Z]+$').hasMatch(value)) {
-      return 'Name must contain only letters';
-      }
-
-      return null;
-      }),
-      const SizedBox(height: 20),
-      _buildTextField('Email', false, _emailController, (value) {
-      if (value == null || value.isEmpty) {
-      return 'Email is required';
-      }
-      if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-      return 'Enter a valid email';
-      }
-      return null;
-      }),
-      const SizedBox(height: 20),
-      _buildTextField('Password', true, _passwordController, (value) {
-      if (value == null || value.isEmpty) {
-      return 'Password is required';
-      }
-      if (value.length < 8) {
-      return 'Password must be at least 8 characters long';
-      }
-
-      return null ;
-      }),
-      const SizedBox(height: 20),
-      _buildTextField('Re-Password', true, _rePasswordController, (value) {
-      if (value == null || value.isEmpty) {
-      return 'Re-Password is required';
-      }
-      if (value != _passwordController.text) {
-      return 'Passwords do not match';
-      }
-      if (value.length < 8) {
-      return 'Re-Password must be at least 8 characters long';
-      }
-      return null;
-      }),
-      const SizedBox(height: 40),
-      CustomButton(
-      text: 'Register',
-      backgroundColor: Colors.orange,
-      onPressed: () {
-      if (_formKey.currentState!.validate()) {
-        final userdata = UserModel(
-          _nameController.text.trim(),
-          _emailController.text.trim(),
-         _passwordController.text.trim(),
-        );
-       BlocProvider.of<AuthCubit>(context).registerUser(userdata: userdata);
-      }
-      },
-      ),
-      ],
-      ),
-      ),
-      ),
-      ),
-      ],
-      ))),
-    ));
-
+    );
   }
 
-  Widget _buildTextField(
-      String label, bool isObscure, TextEditingController controller, String? Function(String?) validator) {
+  Widget _buildTextField(String label, bool isObscure, TextEditingController controller, String? Function(String?) validator) {
     return TextFormField(
       controller: controller,
       obscureText: isObscure,
@@ -178,7 +178,7 @@ void dispose() {
       decoration: InputDecoration(
         labelText: label,
         labelStyle: TextStyle(
-          color: Colors.orange,
+          color: Colors.black,
           fontWeight: FontWeight.bold,
         ),
         filled: true,
@@ -186,6 +186,39 @@ void dispose() {
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(15),
           borderSide: BorderSide.none,
+        ),
+      ),
+      validator: validator,
+    );
+  }
+
+  Widget _buildPasswordField(String label, TextEditingController controller, String? Function(String?) validator) {
+    return TextFormField(
+      controller: controller,
+      obscureText: !_isPasswordVisible,
+      style: TextStyle(color: Colors.white),
+      decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+        ),
+        filled: true,
+        fillColor: Colors.blue[700]!.withOpacity(0.8),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+          borderSide: BorderSide.none,
+        ),
+        suffixIcon: IconButton(
+          icon: Icon(
+            _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            setState(() {
+              _isPasswordVisible = !_isPasswordVisible;
+            });
+          },
         ),
       ),
       validator: validator,
--- lib/features/auth/presentation/views/sign_in_page.dart
+++ lib/features/auth/presentation/views/sign_in_page.dart
@@ -3,96 +3,85 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:task_project/features/auth/data/auth_cubit/auth_cubit.dart';
 import 'package:task_project/features/home/presentation/views/home_page.dart';
+import 'package:task_project/features/auth/presentation/views/register_page.dart'; // Import the register page
 
 import '../../../../core/functions/show_snack_bar.dart';
 import '../../../../core/widgets/custom_button.dart';
 import '../../data/auth_cubit/auth_states.dart';
 
-// SignInPage widget with email and password fields
 class SignInPage extends StatefulWidget {
   @override
   State<SignInPage> createState() => _SignInPageState();
-
 }
 
 class _SignInPageState extends State<SignInPage> {
   final _formKey = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
-
   final TextEditingController _passwordController = TextEditingController();
+  bool isLoading = false;
+  bool _isPasswordVisible = false; // Track password visibility
 
-bool isLoading = false ;
-@override
-void dispose() {
-  _emailController.dispose();
-  _passwordController.dispose();
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<AuthCubit, AuthStates>(
-        listener: (BuildContext context, AuthStates state) {
-          if (state is LoginLoading) {
-            isLoading = true;
-          } else if (state is LoginSuccess) {
-            isLoading = false;
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) =>const HomePage()),
-            );
-          } else if (state is LoginFailure) {
-            isLoading = false;
-            showSnackBar(context, state.errorMessage);
-          }
-        }, builder: (context, state) {
-    return ModalProgressHUD(
-      inAsyncCall :isLoading,
-      child: Scaffold(
-        appBar: AppBar(
-          title:const Text(
-            'Sign In',
-            style: TextStyle(
-              fontSize: 24,
-              fontFamily: 'Pacifico',
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
+      listener: (BuildContext context, AuthStates state) {
+        if (state is LoginLoading) {
+          setState(() => isLoading = true);
+        } else if (state is LoginSuccess) {
+          setState(() => isLoading = false);
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => const HomePage()),
+          );
+        } else if (state is LoginFailure) {
+          setState(() => isLoading = false);
+          showSnackBar(context, state.errorMessage);
+        }
+      },
+      builder: (context, state) {
+        return ModalProgressHUD(
+          inAsyncCall: isLoading,
+          child: Scaffold(
+            appBar: AppBar(
+              title: const Text(
+                'Sign In',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontFamily: 'Pacifico',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              backgroundColor: Colors.blue[900],
+              elevation: 0,
+              centerTitle: true,
             ),
-          ),
-          backgroundColor: Colors.blue[900],
-          elevation: 0,
-          centerTitle: true,
-        ),
-        body: Stack(
-          children: [
-            // Background image
-            // Container(
-            //   decoration: const BoxDecoration(
-            //     image: DecorationImage(
-            //       image: AssetImage('assets/images/background.jpg'),
-            //       fit: BoxFit.cover,
-            //     ),
-            //   ),
-            // ),
-            SingleChildScrollView(
-              child: Padding(
+            body: Center(
+              child: SingleChildScrollView(
                 padding: const EdgeInsets.all(20.0),
                 child: Form(
                   key: _formKey,
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      // Logo or image
                       Container(
-                        height: 150,
-                        width: 150,
-                        decoration:const BoxDecoration(
+                        height: 300,
+                        width: 350,
+                        decoration: const BoxDecoration(
                           image: DecorationImage(
                             image: AssetImage('assets/images/log.jpg'),
                             fit: BoxFit.contain,
                           ),
                         ),
                       ),
-                     const SizedBox(height: 20),
+                      const SizedBox(height: 20),
                       Text(
                         'Welcome Back!',
                         style: TextStyle(
@@ -102,7 +91,6 @@ void dispose() {
                         ),
                       ),
                       const SizedBox(height: 20),
-                      // Email field
                       _buildTextField(
                         'Email',
                         false,
@@ -117,54 +105,73 @@ void dispose() {
                           return null;
                         },
                       ),
-                     const SizedBox(height: 20),
-                      // Password field
+                      const SizedBox(height: 20),
                       _buildTextField(
                         'Password',
-                        true,
+                        _isPasswordVisible,
                         _passwordController,
                             (value) {
                           if (value == null || value.isEmpty) {
                             return 'Password is required';
-
                           }
                           if (value.length < 8) {
                             return 'Password must be at least 8 characters long';
                           }
                           return null;
                         },
+                        isPassword: true,
                       ),
-                     const SizedBox(height: 30),
-                      // Sign In button using CustomButton
+                      const SizedBox(height: 30),
                       CustomButton(
                         text: 'Login',
-                        backgroundColor: Colors.blue[900]!, // Gradient colors
+                        backgroundColor: Colors.blue[900]!,
                         textColor: Colors.white,
                         onPressed: () {
                           if (_formKey.currentState!.validate()) {
-                            // Navigate to the HomePage on successful login
-                          BlocProvider.of<AuthCubit>(context).loginUser(email: _emailController.text.trim(), password: _passwordController.text.trim(),);
+                            BlocProvider.of<AuthCubit>(context).loginUser(
+                              email: _emailController.text.trim(),
+                              password: _passwordController.text.trim(),
+                            );
                           }
                         },
+                      ),
+                      const SizedBox(height: 20),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => RegisterPage()),
+                          );
+                        },
+                        child: const Text(
+                          "Don't have an account? Sign up",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.blue,
+                          ),
+                        ),
                       ),
                     ],
                   ),
                 ),
               ),
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
-    );}
 
-  // Custom text field builder
-  Widget _buildTextField(String label, bool isObscure,
-      TextEditingController controller, String? Function(String?) validator) {
+  Widget _buildTextField(
+      String label,
+      bool isObscure,
+      TextEditingController controller,
+      String? Function(String?) validator, {
+        bool isPassword = false,
+      }) {
     return TextFormField(
       controller: controller,
-      obscureText: isObscure,
+      obscureText: isPassword ? !isObscure : false,
       decoration: InputDecoration(
         labelText: label,
         filled: true,
@@ -173,24 +180,38 @@ void dispose() {
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(15),
           borderSide: BorderSide(
-            color: Colors.blue[900]!, // Border color
+            color: Colors.blue[900]!,
             width: 2,
           ),
         ),
         enabledBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(15),
           borderSide: BorderSide(
-            color: Colors.blue[400]!, // Enabled border color
+            color: Colors.blue[400]!,
             width: 2,
           ),
         ),
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(15),
           borderSide: BorderSide(
-            color: Colors.orange[300]!, // Focused border color
+            color: Colors.orange[300]!,
             width: 2,
           ),
         ),
+        suffixIcon: isPassword
+            ? IconButton(
+          icon: Icon(
+            _isPasswordVisible
+                ? Icons.visibility
+                : Icons.visibility_off,
+          ),
+          onPressed: () {
+            setState(() {
+              _isPasswordVisible = !_isPasswordVisible;
+            });
+          },
+        )
+            : null,
       ),
       validator: validator,
     );
